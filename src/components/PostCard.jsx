e976942d58906a90e6a3a28b2eb3371219ac5070
Mohit Bharti <mohitbharti373@gmail.com>
2025-03-23 22:11:42 +0530

    created post form

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import appwriteService from "../appwrite/config"
+import appwriteService from "../appwrite/conf"
 import {Link} from 'react-router-dom'
 
 function PostCard({$id, title, featuredImage}) {
