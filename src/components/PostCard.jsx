78c0b8718c96c5ff9ea1dc2ce68b87c92139a3bf
Mohit Bharti <mohitbharti373@gmail.com>
2025-03-31 01:00:45 +0530

    refactor PostCard component for improved image handling and layout

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import appwriteService from "../appwrite/conf"
-import {Link} from 'react-router-dom'
+import React from "react";
+import appwriteService from "../appwrite/conf";
+import { Link } from "react-router-dom";
 
-function PostCard({$id, title, featuredImage}) {
-    
+function PostCard({ $id, title, featuredImage }) {
   return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4'>
-            <div className='w-full justify-center mb-4'>
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-                className='rounded-xl' />
-
-            </div>
-            <h2
-            className='text-xl font-bold'
-            >{title}</h2>
+      <div className="w-full bg-gray-100 rounded-xl p-4">
+        <div className="w-full h-48 overflow-hidden mb-4">
+          <img
+            src={appwriteService.getFilePreview(featuredImage)}
+            alt={title}
+            className="w-full h-full object-cover object-center rounded-xl"
+          />
         </div>
+        <h2 className="text-xl font-bold">{title}</h2>
+      </div>
     </Link>
-  )
+  );
 }
 
-
-export default PostCard
+export default PostCard;
