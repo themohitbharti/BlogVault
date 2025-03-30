7ccc336e051fb2ceb939689fe341f506cdfef1e6
Mohit Bharti <mohitbharti373@gmail.com>
2025-03-31 01:25:34 +0530

    fix: pass post object correctly to PostForm component in EditPost

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -22,7 +22,7 @@ function EditPost() {
   return post ? (
     <div className='py-8'>
         <Container>
-            <PostForm {...post} />
+            <PostForm post={post} />
         </Container>
     </div>
   ) : null
